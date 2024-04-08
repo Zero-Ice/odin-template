f7abfd1af20b5b6552d514268865040b9a5b6903
ruipeng <ongruipeng@gmail.com>
2024-04-08 17:32:47 +0800

    Removed files for template

--- src/project-list-item-view.js
+++
@@ -1,9 +0,0 @@
-function getProjectListItemView(project) {
-  const btn = document.createElement("button");
-  btn.classList = "projectlisting-button text";
-  btn.innerText = project.name;
-
-  return btn;
-}
-
-export { getProjectListItemView };
